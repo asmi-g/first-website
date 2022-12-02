23947ef599e78dccf5ccb47566fc0895e54b51dc
asmi-g <108370938+asmi-g@users.noreply.github.com>
2022-12-02 02:16:18 -0500

    first-website

---
+++ personal-portfolio.js
@@ -0,0 +1,91 @@
+const home_hover = document.getElementById('home_hover');
+
+home_hover.addEventListener('mouseover', function handleMouseOver()
+{
+    home_hover.style.color = '#6247aa';
+    home_hover.style.paddingBlock = "5px";
+    home_hover.style.paddingLeft = "5px";
+    home_hover.style.paddingRight = "5px";
+    home_hover.style.backgroundColor =  '#E2CFEA';
+}
+);
+
+home_hover.addEventListener('mouseout', function handleMouseOut()
+{
+    home_hover.style.color = 'white';
+    home_hover.style.paddingBlock = "0px";
+    home_hover.style.paddingLeft = "0px";
+    home_hover.style.paddingRight = "0px";
+    home_hover.style.backgroundColor =  'transparent';
+}
+);
+
+
+const portfolio_hover = document.getElementById('portfolio_hover');
+
+portfolio_hover.addEventListener('mouseover', function handleMouseOver()
+{
+    portfolio_hover.style.color = '#6247aa';
+    portfolio_hover.style.paddingBlock = "5px";
+    portfolio_hover.style.paddingLeft = "5px";
+    portfolio_hover.style.paddingRight = "5px";
+    portfolio_hover.style.backgroundColor =  '#E2CFEA';
+}
+);
+
+portfolio_hover.addEventListener('mouseout', function handleMouseOut()
+{
+    portfolio_hover.style.color = 'white';
+    portfolio_hover.style.paddingBlock = "0px";
+    portfolio_hover.style.paddingLeft = "0px";
+    portfolio_hover.style.paddingRight = "0px";
+    portfolio_hover.style.backgroundColor =  'transparent';
+}
+);
+
+
+const about_hover = document.getElementById('about_hover');
+
+about_hover.addEventListener('mouseover', function handleMouseOver()
+{
+    about_hover.style.color = '#6247aa';
+    about_hover.style.paddingBlock = "5px";
+    about_hover.style.paddingLeft = "5px";
+    about_hover.style.paddingRight = "5px";
+    about_hover.style.backgroundColor =  '#E2CFEA';
+}
+);
+
+about_hover.addEventListener('mouseout', function handleMouseOut()
+{
+    about_hover.style.color = 'white';
+    about_hover.style.paddingBlock = "0px";
+    about_hover.style.paddingLeft = "0px";
+    about_hover.style.paddingRight = "0px";
+    about_hover.style.backgroundColor =  'transparent';
+}
+);
+
+
+
+const contact_hover = document.getElementById('contact_hover');
+
+contact_hover.addEventListener('mouseover', function handleMouseOver()
+{
+    contact_hover.style.color = '#6247aa';
+    contact_hover.style.paddingBlock = "5px";
+    contact_hover.style.paddingLeft = "5px";
+    contact_hover.style.paddingRight = "5px";
+    contact_hover.style.backgroundColor =  '#E2CFEA';
+}
+);
+
+contact_hover.addEventListener('mouseout', function handleMouseOut()
+{
+    contact_hover.style.color = 'white';
+    contact_hover.style.paddingBlock = "0px";
+    contact_hover.style.paddingLeft = "0px";
+    contact_hover.style.paddingRight = "0px";
+    contact_hover.style.backgroundColor =  'transparent';
+}
+);
